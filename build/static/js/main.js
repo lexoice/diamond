9bca403f21541dba2948ae1955d6e6dce2a9436c
Alexey <lexoice@gmail.com>
2018-10-12 15:54:02 +0500

    faq script comlite

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -3,3 +3,21 @@ $('.slider').slick({
   arrows: false,
   slidesToShow: 1,
 });
+
+
+$('.slider2').slick({
+  arrows: false,
+  slidesToShow: 6,
+  autoplay: true,
+  centerMode: true,
+  infinite: true,
+  adaptiveHeight: true,
+});
+
+$(document).ready(function () {
+
+	$('.panel-heading').click(function () {
+		$(this).toggleClass('in').next().slideToggle();
+		$('.panel-heading').not(this).removeClass('in').next().slideUp();
+	});
+});
