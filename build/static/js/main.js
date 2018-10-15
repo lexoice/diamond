37a4bae2ab40d03ad77360cabb7d79c07f387df5
Alexey <lexoice@gmail.com>
2018-10-15 09:03:23 +0500

    adaptive

--- build/static/js/main.js
+++ build/static/js/main.js
@@ -4,6 +4,29 @@ $('.slider').slick({
   slidesToShow: 1,
 });
 
+$('.slider_2').slick({
+  dots: false,
+  arrows: false,
+  slidesToShow: 1,
+});
+
+$('.slider_3').slick({
+  dots: false,
+  arrows: false,
+  slidesToShow: 1,
+});
+
+
+ 
+
+$(document).ready(function () {
+
+	$('.panel-heading').click(function () {
+		$(this).toggleClass('in').next().slideToggle();
+		$('.panel-heading').not(this).removeClass('in').next().slideUp();
+	});
+});
+
 
 $('.slider2').slick({
   arrows: false,
@@ -12,12 +35,31 @@ $('.slider2').slick({
   centerMode: true,
   infinite: true,
   adaptiveHeight: true,
-});
-
-$(document).ready(function () {
-
-	$('.panel-heading').click(function () {
-		$(this).toggleClass('in').next().slideToggle();
-		$('.panel-heading').not(this).removeClass('in').next().slideUp();
-	});
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
